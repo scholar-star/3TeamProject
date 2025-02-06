cf7bef8164acb2a8c242a4fd2bc6d8b96d75ada4
scholar-star <cheve1224@pusan.ac.kr>
2025-02-06 22:48:12 +0900

    front에서 로그인/회원가입 분할, database dependency
    하지만 아직 Database Table 구축에서 문제가 발생한 것이 고쳐지지 않아서 main 파일은 홈에서 적용되는 로그인 연결까지 완성했습니다

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
+import sys
+sys.path.extend('./')
+
+from apps.dependencies import get_db_session, create_db_and_tables
 from fastapi import FastAPI, HTTPException, status, Depends, Request
 from fastapi.templating import Jinja2Templates
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Session, create_engine, select, Field
 from dataclasses import dataclass, asdict
 from enum import Enum
 import time
+import bcrypt
 
+templates = Jinja2Templates(directory="front")
 
-templates = Jinja2Templates(directory="html")
+@dataclass
+class SignupReq(SQLModel, table=True):
+    id: str | None = Field(default=None, primary_key=True)
+    username: str
+    password: str
+    name: str
+    phone: str|None = None
+    email: str|None = None
+
+@dataclass
+class SigninReq:
+    username: str
+    password: str
+
 app = FastAPI()
 
-
 login_session = {}
-
-
 
 @dataclass
 class Post:
@@ -30,7 +46,9 @@ class CreatePostReq:
     author: str
     publish: bool = True
 
-
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get("/home_page")
 def open_home_page(request: Request):
@@ -39,7 +57,6 @@ def open_home_page(request: Request):
 @app.get("/write_page")
 def open_write_page(request: Request):
     return templates.TemplateResponse("write_page.html", {"request":request})
-
 
 @app.post("/write_page")
 def create_post(cPost: CreatePostReq):
@@ -51,5 +68,35 @@ def create_post(cPost: CreatePostReq):
                  published=cPost.publish)
     print(result)
 
-
     return {"ok": True}
+
+@app.post("/signup_page")
+def login(req: SigninReq):
+    return {
+        "token": "This is token.",
+        "detail": None,
+        "ok": True
+    }
+
+@app.get("/signup_page")
+def viewLogin(req: Request):
+    return templates.TemplateResponse("login_page.html", {"request": req})
+
+@app.post("/signup")
+def signup(req: SignupReq, session=Depends(get_db_session)):
+    crypted_password = bcrypt.hashpw(req.password.encode('utf-8'), bcrypt.gensalt())
+    req.password = crypted_password
+    try:
+        session.add(req)
+        session.commit() # 트랜잭션
+        session.refresh(req)
+    except:
+        raise HTTPException(status_code=500, detail="Failed to create user")
+    return {
+        "detail": "None"
+    }
+
+@app.get("/signup")
+def viewSignup(req: Request):
+    return templates.TemplateResponse("signup_page.html", {"request": req})
+ 
