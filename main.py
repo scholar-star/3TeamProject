33bed36fdaef6f31d4690816d3a383491f96b18a
scholar-star <cheve1224@pusan.ac.kr>
2025-02-07 15:58:50 +0900

    main-Jeong
    회원관리 & 로그인 부분(로그아웃 제외) 완성

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ sys.path.extend('./')
 from apps.dependencies import get_db_session, create_db_and_tables
 from fastapi import FastAPI, HTTPException, status, Depends, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
 from sqlmodel import SQLModel, Session, create_engine, select, Field
 from dataclasses import dataclass, asdict
 from enum import Enum
@@ -14,8 +15,7 @@ templates = Jinja2Templates(directory="front")
 
 @dataclass
 class SignupReq(SQLModel, table=True):
-    id: str | None = Field(default=None, primary_key=True)
-    username: str
+    username: str = Field(primary_key=True)
     password: str
     name: str
     phone: str|None = None
@@ -71,12 +71,20 @@ def create_post(cPost: CreatePostReq):
     return {"ok": True}
 
 @app.post("/signup_page")
-def login(req: SigninReq):
-    return {
-        "token": "This is token.",
-        "detail": None,
-        "ok": True
-    }
+def login(req: SigninReq, session=Depends(get_db_session)):
+    state = select(SignupReq).where(SignupReq.username == req.username)
+    person = session.exec(state).first()
+    if not person:
+        raise HTTPException(status_code = 404, detail="User not found")
+    
+    if bcrypt.checkpw(req.password.encode('utf-8'), person.password):
+        return {
+            "token": "This is token.",
+            "detail": None,
+            "ok": True
+        }
+    else:
+        raise HTTPException(status_code=401, detail="Incorrect password")
 
 @app.get("/signup_page")
 def viewLogin(req: Request):
@@ -86,14 +94,16 @@ def viewLogin(req: Request):
 def signup(req: SignupReq, session=Depends(get_db_session)):
     crypted_password = bcrypt.hashpw(req.password.encode('utf-8'), bcrypt.gensalt())
     req.password = crypted_password
+    print(req)
     try:
         session.add(req)
         session.commit() # 트랜잭션
         session.refresh(req)
-    except:
+    except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail="Failed to create user")
     return {
-        "detail": "None"
+        "ok":True
     }
 
 @app.get("/signup")
