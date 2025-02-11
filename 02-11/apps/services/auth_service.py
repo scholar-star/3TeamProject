63f10a0143eef1a9e29f816a3477a8453fee731e
scholar-star <cheve1224@pusan.ac.kr>
2025-02-11 11:12:54 +0900

    02-11자 코드 개선, 수정
    auth_service로 비즈니스 로직을 보낸 뒤 회원가입 과정에서도 front에서 제약을 걸도록 했다.

---
+++ 02-11/apps/services/auth_service.py
@@ -0,0 +1,47 @@
+from enum import Enum
+from fastapi import Depends, HTTPException
+from apps.models.author import SignupReq, SigninReq, SigninResp
+from apps.dependencies.dependencies import get_db_session
+from sqlmodel import select, Session
+import bcrypt, time
+from jose import jwt
+
+class RESULT_CODE(Enum):
+    NOT_FOUND = "Not Found"
+    FAILED = "Failed"
+    SUCCESS = "success"
+
+SECRET_KEY = 'eFgxd67V1cmuXTTq6GuCMBfMWuJiNcYDprvq4'
+ALGORITHM = 'HS256'
+
+class AuthService:
+    def login_service(self,db:Session, username:str, password:str):
+        state = select(SignupReq).where(SignupReq.username == username)
+        person = db.exec(state).first()
+        if not person:
+            raise HTTPException(status_code = 404, detail="User not found")
+        
+        if (bcrypt.checkpw(password.encode('utf-8'), person.password) and person.username == username):
+            payload = {
+                "id": person.username,
+                "exp": time.time() + 60*30
+            }
+            token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+            return SigninResp(token=token, detail=None)
+        else:
+            raise HTTPException(status_code=401, detail="Login failed")
+
+    def signup_service(self, db:Session, req: SignupReq):
+        crypted_password = bcrypt.hashpw(req.password.encode('utf-8'), bcrypt.gensalt())
+        req.password = crypted_password
+        try:
+            db.add(req)
+            db.commit() # 트랜잭션
+            db.refresh(req)
+        except Exception as e:
+            print(e)
+            raise HTTPException(status_code=500, detail="Failed to create user")
+        return {
+            "ok":True
+        }
+
